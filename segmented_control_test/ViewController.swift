5473efb44de29751927d5bbed906b721a8597e9c
shinohara-san <someiyoshino0809@gmail.com>
2020-09-10 10:16:16 +0900

    Initial Commit

---
+++ segmented_control_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segmented_control_test
+//
+//  Created by Yuki Shinohara on 2020/09/10.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
